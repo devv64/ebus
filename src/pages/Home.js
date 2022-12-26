30f7e79b1b2e6a2aff9fb00c514c2970e8d6ec9c
Dev <devextraemail@gmail.com>
2022-12-26 03:50:07 -0500

    first commit baby

---
+++ src/pages/Home.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import './styles.css';
+import schoolImg from './school.png';
+import { Link, useNavigate } from 'react-router-dom'
+
+function Home() {
+
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      <nav className="navbar">
+        <div className="navbar__container">
+          <a href="/" id="navbar__logo"><i className="fas fa-bus"></i>eBUS</a>
+          <div className="navbar__toggle" id="mobile-menu">
+            <span className="bar"></span>
+            <span className="bar"></span>
+            <span className="bar"></span>
+          </div> 
+          <ul className="navbar__menu">
+            <li className="navbar__item">
+              <a href="/" className="navbar__links">Home</a>
+            </li>
+            <li className="navbar__item">
+              <a href="about" className="navbar__links">About</a>
+            </li>
+            <li className="navbar__item">
+              <a href="support" className="navbar__links">Support</a>
+            </li>
+            <li className="navbar__btn">
+              <a className="navbar__links" onClick={() => {navigate("/signup")}}>Login</a>
+              </li>
+          </ul>
+        </div>
+      </nav>
+
+      <div className="main">
+        <div className="main__container">
+          <div className="main__content">
+            <h1>Welcome To</h1>
+            <h2>The New Age of Transportation</h2>
+            <p>Schools ┃ Students ┃ Bus Drivers</p>
+            <button className="main__btn"><a onClick={() => {navigate("/signup")}}>Get Started</a></button>
+          </div>
+          <div className="main__img--container">
+            <img src={schoolImg} alt="pic" id="main__img" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/pages/Signup.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Signup() {
+  return (
+    <div>
+        <button class="school__btn"><a href="/">School</a></button>
+        <button class="student__btn"><a href="signup.html">Student</a></button>
+        <button class="bus__btn"><a href="/">Bus Driver</a></button>
+    </div>
+  );
+}
+
+export default Signup;
